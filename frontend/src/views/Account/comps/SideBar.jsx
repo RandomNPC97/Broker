dd0ca9b4aa8f53798183cc26307752a2631c9d9b
RandomNPC97 <randomnpc1997@gmail.com>
2025-06-03 12:50:13 +0100

    Update SideBar.jsx

--- frontend/src/views/Account/comps/SideBar.jsx
+++ frontend/src/views/Account/comps/SideBar.jsx
@@ -93,7 +93,7 @@ const SideBar = () => {
 
           {/* Profile Name */}
           <div>
-            <h1 className="pt-4 text-[14px] text-center">{User.deposit_balance}</h1>
+            <h1 className="pt-4 text-[14px] text-center">{User.full_name}</h1>
           </div>
         </div>
 
